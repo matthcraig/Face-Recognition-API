98d298b4a6ff4c2a61c7ea2fbe9c244fe0b8ae21
Matthew Craig <mgcraig78@gmail.com>
2020-12-07 20:03:11 -0800

    initial commit

--- server.js
+++ server.js
@@ -10,13 +10,12 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex({
-  // connect to your own database here
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'aneagoie',
+    user : 'matthewcraig',
     password : '',
-    database : 'smart-brain'
+    database : 'smart-brain2'
   }
 });
 
@@ -32,6 +31,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(3001, ()=> {
+  console.log('app is running on port 3001');
 })
